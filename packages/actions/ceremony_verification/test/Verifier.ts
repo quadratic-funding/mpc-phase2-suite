33f6c3a5cf85825f66a74c6de30207665b246e49
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2023-02-28 19:32:42 +0000

    feat(verification env): created env for proof verification
    Created an Hardhat project within the actions package configured to allow verification that the
    Verifier contract works correctly.

---
+++ packages/actions/ceremony_verification/test/Verifier.ts
@@ -0,0 +1,12 @@
+// import { expect } from "chai";
+// import { ethers } from "hardhat";
+
+describe("P0T1ON generated Verifier", () => {
+    describe("Deployment", () => {
+        it("should deploy the contract", async () => {})
+    })
+    describe("Proof verification", () => {
+        it("should verify a valid SNARK proof", async () => {})
+        it("should throw when provided an invalid proof", async () => {})
+    })
+})
